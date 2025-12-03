ba933cc7cbfd684215378a87df5a1b09e691d7e5
Admin <georg-edward.piil@voco.ee>
2025-12-03 05:43:38 -0800

    header ja vaated

---
+++ project/index.js
@@ -0,0 +1,56 @@
+// import { Product } from "./product.js";
+// import { Cart } from "./cart.js";
+// import { Order } from "./order.js";
+// import { Customer } from "./customer.js";
+
+// const laptop = new Product("Laptop", 999.99, "Electronics");
+// const phone = new Product("Smartphone", 499.99, "Electronics");
+
+// const myCart = new Cart();
+// myCart.addProduct(laptop, 1);
+// myCart.addProduct(phone, 2);
+
+// console.log(`Total Items: ${myCart.totalItems}`);
+// console.log(`Total Price: $${myCart.calculateTotal().toFixed(2)}`);
+
+// myCart.removeProduct(phone.id);
+// console.log(`Total Items in Cart after removal: ${myCart.totalItems}`);
+// console.log(
+//   `Total Price after removal: $${myCart.calculateTotal().toFixed(2)}`
+// );
+
+// const myOrder = new Order(myCart);
+// myOrder.printOrder();
+
+// const customer1 = new Customer("Alice");
+// customer1.placeOrder(myCart);
+// customer1.printOrderHistory();
+
+// //STEP 1 CREATE ELEMENT 
+// const newH1 = document.createElement.createElement("h1")
+
+// // STEP 2 ADD ATTRIBUTES/PROPERTIES
+// newH1. textContent = "I like pizza!"
+// newH1.id = "myH1";
+// newH1.style.color = "tomato";
+// newH1.style.textAlign = "center";
+
+
+// //STEP 3 APPEND ELEMENT TO DOM 
+
+// document.body.prepend(newH1);
+// document.body.prepend(newH1);
+// document.getElementById("box1").prepend(newH1);
+// document.getElementById("box1").append(newH1);
+
+const header = document.getElementById('header');
+header.style.background = '#333' 
+header.style.color = 'white';
+header.style.padding = '20px';
+header.style.display = 'flex';
+header.style.justifyContent = 'space-between';
+header.style.alignItems = 'center';
+
+const logo = document.createElement('h1');
+logo.textContent = 'ePood';
+logo.style.margin = '0';
---
+++ views/allProductsView.js
@@ -0,0 +1,14 @@
+export function displayProductCard(product) {
+    const card = document.createElement('div');
+    card.classList.add('product-card');
+
+    const card = document.createElement('div');
+    card.classList.add('product-card');
+
+
+    const title = document.createElement('h2');
+    title.textContent = product.title;
+    card.appendChild(title);
+
+    const category
+}
