b3f3cf49175cad6b111d22c360f54ac2b65b4574
Admin <georg-edward.piil@voco.ee>
2025-12-16 23:11:56 -0800

    modulaarne epood

--- views/allProductsView.js
+++ views/allProductsView.js
@@ -2,13 +2,16 @@ export function displayProductCard(product) {
     const card = document.createElement('div');
     card.classList.add('product-card');
 
-    const card = document.createElement('div');
-    card.classList.add('product-card');
-
 
     const title = document.createElement('h2');
     title.textContent = product.title;
     card.appendChild(title);
 
-    const category
+    const category = document.createElement('p');
+    category.textContent = `Category ${product.category}`;
+    card.appendChild(category);
+
+    const price = document.createElement('p');
+    price.textContent = `$${product.price.toFixed(2)}`;
+    price.classList.add('product-price');
 }
